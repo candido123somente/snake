466a512b4c58f927223eea277356c18e6d3d3b09
Candido Ferreira <candido_24@hotmail.com>
2020-06-25 21:07:21 -0300

    adicionando movimentacao e borda infinita

--- game.js
+++ game.js
@@ -1,26 +1,61 @@
 let stage = document.getElementById("GameCanvas");
 let context = stage.getContext("2d");
-setInterval(inGame, 60);
+this.document.addEventListener("keydown", keyPush);
 
+setInterval(inGame, 30);
 
 let boardSize = 20;
+let speed = 5;
 
-var snakeX = 5;
-var snakeY = 5;
+let snakeX = 1;
+let snakeY = 1;
 
-var appleX = Math.floor(Math.random()*boardSize);
-var appleY = Math.floor(Math.random()*boardSize);
+let appleX = 1;
+let appleY = 1;
 
 function inGame(){
-
+this.document.addEventListener("keydown", keyPush);
 context.fillStyle = "black";
-context.fillRect(2,2,596,396);
+context.fillRect(0,0,600,400);
 
 context.fillStyle = "green";
-context.fillRect(snakeX*boardSize, snakeY*boardSize, boardSize, boardSize);
+context.fillRect(snakeX, snakeY, boardSize, boardSize);
 
 context.fillStyle = "red";
-context.fillRect(appleX*boardSize, appleY*boardSize, boardSize, boardSize);
+context.fillRect(appleX, appleY, boardSize, boardSize);
+
+if (snakeX >= stage.width){
+    snakeX = 0 - boardSize;
+}
+if (snakeX <= 0 - 2*boardSize){
+    snakeX = stage.width - 1;
+}
+if (snakeY > stage.height){
+    snakeY = 0 - boardSize;
+}
+if (snakeY < 0 - 2*boardSize){
+    snakeY = stage.height;
+}
 
 
+}
+
+function keyPush(event){
+
+    switch (event.keyCode){
+        case 37: // tecla da esquerda
+        snakeX = snakeX - speed;
+        break;
+        case 38: // tecla de cima
+        snakeY = snakeY - speed;
+        break;
+        case 39: // tecla de direita
+        snakeX = snakeX + speed;
+        break;
+        case 40: // tecla de baixo
+        snakeY = snakeY + speed;
+        break;
+        default:
+            break;
+    }
 }
