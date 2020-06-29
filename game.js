5a3de2243be845222e583c9feb3e27ebea2e8f95
Candido Ferreira <candido_24@hotmail.com>
2020-06-29 20:59:09 -0300

    Adcionando um menu inicial

--- game.js
+++ game.js
@@ -1,13 +1,12 @@
 let stage = document.getElementById("GameCanvas");
 let context = stage.getContext("2d");
 document.addEventListener("keydown", keyPush);
-setInterval(inGame, 60);
+window.onload = currentScreen;
 
 
 let boardPieceSize = 20;
 let boardAmountX = 30;
 let boardAmountY = 20;
-
 let speed = 1/2;
 let speedX = 1/2;
 let speedY = 0;
@@ -22,6 +21,19 @@ let appleY = Math.floor(Math. random() * 19);
 
 let points = 0;
 
+function currentScreen(){ 
+           menuGame();
+}
+function menuGame(){
+    context.fillStyle = "black";
+    context.fillRect(0,0, 600, 400);
+    context.fillStyle = "white";
+    context.font = "45px Helveltica";
+    context.fillText("Snake", 220, 180);
+    context.font = "15px Helveltica";
+    context.fillText("Press space to start", 220, 210);
+}
+
 function inGame(){
 
 context.fillStyle = "black";
@@ -29,9 +41,6 @@ context.fillRect(0,0,600,400);
 
 context.fillStyle = "red";
 context.fillRect(appleX*boardPieceSize, appleY*boardPieceSize, boardPieceSize, boardPieceSize);
-
-
-
 context.fillStyle = "green";
 for (var i = 0; i < drag.length; i++){
     context.fillRect(drag[i].dragX*boardPieceSize, drag[i].dragY*boardPieceSize, boardPieceSize, boardPieceSize);
@@ -43,36 +52,35 @@ context.fillText(points, 580, 15);
 
 drag.push(
     {dragX:snakeX, dragY: snakeY}
-)
-while (drag.length > tail){
-    drag.shift();
+    )
+    while (drag.length > tail){
+        drag.shift();
+    }
+    
+    snakeX = snakeX + speedX;
+    snakeY = snakeY + speedY;
+    
+    
+    if (snakeX > boardAmountX -1){ 
+        snakeX = 0;
+    }
+    if (snakeX < 0){
+        snakeX = boardAmountX - 1;
+    }
+    if (snakeY > boardAmountY -1){
+        snakeY = 0;
+    }
+    if (snakeY < 0){
+        snakeY = boardAmountY - 1;
+    }
+    
+    if (snakeX == appleX && snakeY == appleY){
+        points = points + 1;
+        tail = tail + 1;
+        appleX = Math.floor(Math.random() * 29);
+        appleY = Math.floor(Math. random() * 19);
+    
 }
-
-snakeX = snakeX + speedX;
-snakeY = snakeY + speedY;
-
-
-if (snakeX > boardAmountX -1){ 
-    snakeX = 0;
-}
-if (snakeX < 0){
-    snakeX = boardAmountX - 1;
-}
-if (snakeY > boardAmountY -1){
-    snakeY = 0;
-}
-if (snakeY < 0){
-    snakeY = boardAmountY - 1;
-}
-
-if (snakeX == appleX && snakeY == appleY){
-    points = points + 1;
-    tail = tail + 1;
-    appleX = Math.floor(Math.random() * 29);
-    appleY = Math.floor(Math. random() * 19);
-   
-}
-
 
 
 
@@ -97,6 +105,9 @@ function keyPush(event){
         speedX = 0;
         speedY = + speed;
         break;
+        case 32:
+            setInterval(inGame, 60);
+            break;
         default:
             break;
     }
